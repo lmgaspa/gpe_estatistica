ac976ef5586568b58c5366763a21a89c8d4ea92f
Luiz <seu-email@example.com>
2024-07-19 19:00:23 -0300

    final version

--- pages/aplicativo.tsx
+++ pages/aplicativo.tsx
@@ -5,22 +5,22 @@ import googlePlayBadge from '../public/googlebadge.png';
 
 const AplicativoPage: React.FC = () => {
   return (
-    <div className="flex flex-col col:flex-row mx-auto w-full col:w-5/6 items-center justify-center p-4 col:p-6">
-      <div className="flex items-center justify-center col:justify-between w-full">
-        <div className="flex-1 col:w-1/2 text-center col:text-left p-4 col:pl-12">
-          <h1 className="text-2xl col:text-4xl font-bold mb-4">Fazer entrevista nunca foi tão fácil</h1>
-          <p className="text-base col:text-lg mb-6">
+    <div className="flex flex-col lg:flex-row mx-auto w-full lg:w-5/6 items-center justify-center p-4 lg:p-6">
+      <div className="flex flex-col lg:flex-row items-center lg:justify-between w-full">
+        <div className="flex-1 lg:w-1/2 text-center lg:text-left p-4 lg:pl-12">
+          <h1 className="text-2xl lg:text-4xl font-bold mb-4">Fazer entrevista nunca foi tão fácil</h1>
+          <p className="text-base lg:text-lg mb-6">
             Tudo que você precisa para fazer entrevista agora na palma da sua mão.
             Disponível gratuitamente no Google Play. Experimente!
           </p>
-          <div className="flex justify-center col:justify-start">
+          <div className="flex justify-center lg:justify-start">
             <a href="https://play.google.com/store/apps/details?id=br.com.gpebahia&hl=en&gl=US&pli=1" target="_blank" rel="noopener noreferrer">
-              <Image src={googlePlayBadge} alt="Disponível no Google Play" className="w-32 col:w-48 lg:w-60" />
+              <Image src={googlePlayBadge} alt="Disponível no Google Play" className="w-32 lg:w-48" />
             </a>
           </div>
         </div>
-        <div className="flex-1 col:w-1/2 flex justify-center p-4 col:pr-12">
-          <Image src={appImage} alt="App Image" className="w-full h-auto col:scale-125 col:scale-100 lg:scale-75 xl:scale-75" />
+        <div className="flex-1 lg:w-1/2 flex justify-center p-4 lg:pr-12">
+          <Image src={appImage} alt="App Image" className="w-full max-w-sm scale-150" />
         </div>
       </div>
     </div>
@@ -28,4 +28,3 @@ const AplicativoPage: React.FC = () => {
 };
 
 export default AplicativoPage;
-
