3ea0374f7a83ea235c613f2d24f261b2e8fdd36a
Luiz <seu-email@example.com>
2024-08-07 16:09:40 -0300

    fix padding in navbar

--- pages/app.tsx
+++ pages/app.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head';
-import ContatoPage from "./contato"
 import Eleicoes2024Page from "./eleicoes"
 import InstitutoPage from "./instituto"
-import AplicativoPage from "./aplicativo"
 import DenunciePage from "./denuncie"
+import ContatoPage from "./contato"
+import AplicativoPage from "./aplicativo"
 
 const App: React.FC = () => {
     return (
