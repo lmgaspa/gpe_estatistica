0a60f6d6f428eff08c2472c0a43b7278a5ab3dc9
Luiz <seu-email@example.com>
2024-08-07 19:19:40 -0300

    fix_img_size_bug

--- pages/aplicativo.tsx
+++ pages/aplicativo.tsx
@@ -6,7 +6,7 @@ import googlePlayBadge from '../public/googlebadge.png';
 const AplicativoPage: React.FC = () => {
   return (
     <div className="flex flex-col lg:flex-row mx-auto w-full lg:w-5/6 items-center justify-center p-4 lg:p-6">
-      <div className="flex flex-col lg:flex-row items-center lg:justify-between w-full lg:ml-12">
+      <div className="flex flex-col lg:flex-row items-center lg:justify-between w-full lg:ml-12 pb-12 mb-12">
         <div className="flex-1 lg:w-1/2 text-center lg:text-left p-4 lg:ml-12 lg:pl-12">
           <h1 className="text-2xl lg:text-4xl font-bold mb-4">Fazer entrevista nunca foi tão fácil</h1>
           <p className="text-base lg:text-lg mb-6">
