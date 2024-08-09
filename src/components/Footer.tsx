2917855ab78000384094ac989f95c7730f484178
Luiz <seu-email@example.com>
2024-08-09 04:29:06 -0300

    final

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-blue-900 p-6 text-white">
       <div className="container mx-auto flex flex-col items-center justify-center text-center">
-        <p className='hover:text-yellow-300 cursor-pointer'>&copy; {new Date().getFullYear()} GPE Estatística. Todos os direitos reservados.</p>
+        <p className='hover:text-yellow-300 cursor-default'>&copy; {new Date().getFullYear()} GPE Estatística. Todos os direitos reservados.</p>
         <div className="flex flex-col sm:flex-row mt-4 space-y-4 sm:space-y-0 sm:space-x-4">
           <a href="mailto:gpebahia@gmail.com" className="flex items-center space-x-2 hover:text-yellow-300">
             <FaEnvelope />
